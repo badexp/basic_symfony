0d3b9d57e41e5bfdcf8e2fad7fda3c5c56d340dc
badexp <i.ushakov98@gmail.com>
2019-03-31 01:36:23 +0200

    Added fixtures with generating 5000 test entities inside it

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,25 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Test;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
+    private const FAKE_TEST_COUNT = 5000;
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $fakeTestCount = self::FAKE_TEST_COUNT;
+        while($fakeTestCount--)
+        {
+            $test = new Test();
+            $test->setTitle('Title');
+            $test->setWeight($fakeTestCount);
+
+            $manager->persist($test);
+        }
 
         $manager->flush();
     }
